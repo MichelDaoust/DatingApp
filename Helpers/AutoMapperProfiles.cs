15986f488dfb0f1969ee5808238cb34dc386423a
Michel Daoust <michel.daoust@cgi.com>
2018-02-01 01:01:29 -0500

    set main photo

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,9 @@ namespace DatingApp.API.Helpers
             
             CreateMap<Photo,PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<Photo, PhotoForReturnDto>();
+
         }
     }
 }
